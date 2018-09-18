1a8abe9d9b715382a495784f693c70d76348f1fc
Hilleriteau Jean <jhi@live.fr>
2018-09-18 23:13:39 +0200

    change the fetch event

--- sw.js
+++ sw.js
@@ -15,11 +15,38 @@ self.addEventListener('install', function (e) {
 });
 
 
-self.addEventListener('fetch', function (e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(async () => {
-    let response = await caches.match(e.request);
-    return response || await fetch(e.request);
-  });
-});
+// self.addEventListener('fetch', function (e) {
+//   console.log('[ServiceWorker] Fetch', e.request.url);
+//   e.respondWith(
+//     async () => {
+//       let response = await caches.match(e.request);
+//       if(response) return response;
+//       return await fetch(e.request);
+//     }
+//   );
+// });
 
+self.addEventListener('fetch', function(event) {
+  console.log('Gestion de l\'évènement de fetch pour', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Réponse trouvée en cache:', response);
+
+        return response;
+      }
+      console.log('Pas de réponse trouvée en cache. Sur le point de la récupérer via le réseau...');
+
+      return fetch(event.request).then(function(response) {
+        console.log('La réponse du réseau est:', response);
+
+        return response;
+      }).catch(function(error) {
+        console.error('Récupération échouée:', error);
+
+        throw error;
+      });
+    })
+  );
+});
